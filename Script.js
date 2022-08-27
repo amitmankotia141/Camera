fbb8338932ecd0e68579181e869ead749adedc11
Amit Mankotia <amit100.mankotia@gmail.com>
2022-08-27 22:51:52 +0530

    record

--- Script.js
+++ Script.js
@@ -1,13 +1,10 @@
-let recordBtn=document.querySelector(".record-btn");
-let recordBtnCont=document.querySelector(".record-btn-cont");
-let captureBtn=document.querySelector(".capture.btn");
-let captureBtnCont=document.querySelector(".capture-btn-cont");
-// captureBtnCont.addEventListener("click",function(){
-//     captureBtn.classList.add("scale-capture");
-//     // setTimeout(()=>{
-//     // captureBtn.classList.remove("scale-capture");
-//     // },1000)
-// })
+const recordBtn=document.querySelector(".record-btn");
+const recordBtnCont=document.querySelector(".record-btn-cont");
+const captureBtn=document.querySelector(".capture.btn");
+const captureBtnCont=document.querySelector(".capture-btn-cont");
 captureBtnCont.addEventListener("click",function(){
-    captureBtn.classList.add("scale-capture");
+captureBtn.classList.add("scale-capture");
+    // setTimeout(()=>{
+    // captureBtn.classList.remove("scale-capture");
+    // },1000)
 })
