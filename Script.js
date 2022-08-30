c0ad1361164c2ff423269696fe1d603bb305ffef
Amit Mankotia <amit100.mankotia@gmail.com>
2022-08-30 08:12:26 +0530

    bts

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 const recordBtn=document.querySelector(".record-btn");
 const recordBtnCont=document.querySelector(".record-btn-cont");
-const captureBtn=document.querySelector(".capture.btn");
+const captureBtn=document.querySelector(".capture-btn");
 const captureBtnCont=document.querySelector(".capture-btn-cont");
 captureBtnCont.addEventListener("click",function(){
 captureBtn.classList.add("scale-capture");
