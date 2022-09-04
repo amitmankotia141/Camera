479b4d4a6dfc09a46916df5f4ef5dc68774f7918
Amit Mankotia <amit100.mankotia@gmail.com>
2022-09-04 11:25:36 +0530

    blob

--- Script.js
+++ Script.js
@@ -4,6 +4,32 @@ const captureBtn = document.querySelector(".capture-btn");
 const captureBtnCont = document.querySelector(".capture-btn-cont");
 const timerCont=document.querySelector(".timer-cont");
 const timer=document.querySelector(".timer");
+const video=document.querySelector("video")
+const constraints={
+    video:true,
+    audio:true
+}
+// let mediaRecorder;
+const chunks=[];
+navigator.mediaDevices.getUserMedia(constraints).then((stream)=>{
+video.srcObject=stream;
+const mediaRecorder=new MediaRecorder(stream);
+mediaRecorder.addEventListener("start",function(){
+    console.log("rec started");
+})
+mediaRecorder.addEventListener("dataavailable",function(e){
+chunks.push(e.data);
+})
+mediaRecorder.addEventListener("stop",function(){
+    let blob=new Blob(chunks,{type:"video/mp4"});
+    console.log("rec stopped");
+    let videoURL=URL.createObjectURL(blob);
+    console.log(videoURL);
+    let a=document.createElement("a");
+    a.href=videoURL;
+    a.download="My mp4"
+    a.click();
+})
 captureBtnCont.addEventListener("click", function () {
     captureBtn.classList.add("scale-capture");
     setTimeout(() => {
@@ -14,11 +40,14 @@ let isRecording=false;
 recordBtnCont.addEventListener("click", function () {
     if(!isRecording){
     recordBtn.classList.add("scale-record");
+    mediaRecorder.start();
     timer.style.display="flex";
 }
 else{
     recordBtn.classList.remove("scale-record");
+    mediaRecorder.stop();
     timer.style.display="none";
 }
 isRecording=!isRecording;
+})
 })
